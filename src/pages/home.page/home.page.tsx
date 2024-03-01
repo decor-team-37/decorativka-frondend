c9b8603bc3af24b6a1f5e5e104fc1275304d5397
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2024-03-01 15:28:48 +0200

    add section about and switch name folder from posters to img

--- src/pages/home.page/home.page.tsx
+++ src/pages/home.page/home.page.tsx
@@ -1,3 +1,4 @@
+import { About } from './components/About';
 import { DecorIntroduction } from './components/DecorIntroduction';
 
 import './home.page.scss';
@@ -6,6 +7,8 @@ export const HomePage = () => {
   return (
     <main className='home-page'>
       <DecorIntroduction />
+      <About />
+      <About />
     </main>
   );
 };
