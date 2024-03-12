fe683aa0690e7b11491e3769cdc96f8a60235c5e
Vadym Bavorovskyi <vadymbavorovskiy@gmail.com>
2024-03-12 16:03:01 +0200

    add product page fisrt vesion

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,16 +1,16 @@
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { App } from './App';
-import { HomePage } from './pages/home.page';
+// import { HomePage } from './pages/home.page';
 import { NotFoundPage } from './pages/not-found-page';
 import { Cooperation } from './pages/cooperation-page';
-// import { ProductPage } from './pages/product-page/product-page';
+import { ProductPage } from './pages/product-page/product-page';
 
 export const Root = () => (
   <Router>
     <Routes>
       <Route path="/" element={<App />}>
-        {/* <Route index element={<ProductPage />} /> */}
-        <Route index element={<HomePage />} />
+        <Route index element={<ProductPage />} />
+        {/* <Route index element={<HomePage />} /> */}
         <Route path="cooperation" element={<Cooperation />} />
 
         <Route path="*" element={<NotFoundPage />} />
